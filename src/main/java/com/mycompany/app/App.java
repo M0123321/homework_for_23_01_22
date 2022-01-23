f4a0a82c20b731dee99a9247d4579147b8606600
M0123321 <88082643+M0123321@users.noreply.github.com>
2022-01-23 22:10:17 +0300

    homework upload

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Тупо вписал сюда хоть что-нибудь
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
